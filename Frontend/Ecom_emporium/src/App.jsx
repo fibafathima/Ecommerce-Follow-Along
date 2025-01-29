7062240f1ff0755cfce097ae33aa4bc922bdfa0a
fiba fathima <fibaaminnu@gmail.com>
2025-01-29 14:57:43 +0530

    added form

--- Frontend/Ecom_emporium/src/App.jsx
+++ Frontend/Ecom_emporium/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/signup";
 import Home from "./components/Home";
+import ProductForm from "./components/ProductForm";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/productForm" element={<ProductForm />} />
       </Routes>   
     </BrowserRouter>
     </>
